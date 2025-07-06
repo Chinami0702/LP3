0e948dba417b3791ad5ea4c3d175869c3d46a17d
Chinami0702 <chinamik0702@gmail.com>
2025-07-06 12:26:10 +0900

    インデント

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,27 +1,27 @@
 new Swiper(".voice__swiper", {
-  loop: false,
-  allowTouchMove: true,
+    loop: false,
+    allowTouchMove: true,
 
-  centeredSlides: true,
-  initialSlide: 1,
+    centeredSlides: true,
+    initialSlide: 1,
 
-  slidesPerView: 1.3,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  autoplay: {
-    delay: 6000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    768: {
-      allowTouchMove: false,
-      slidesPerView: 3,
-      spaceBetween: 10,
-      pagination: false,
+    slidesPerView: 1.3,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  },
+
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        768: {
+            allowTouchMove: false,
+            slidesPerView: 3,
+            spaceBetween: 10,
+            pagination: false,
+        },
+    },
 });
